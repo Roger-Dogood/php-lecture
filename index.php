c042e12ff8571ac6b1ba6de3dc2b634486fa424f
Matthew Dahl <daftdahl@gmail.com>
2015-02-28 21:38:00 -0800

    Cleaned up the HTML a little.

--- index.php
+++ index.php
@@ -18,16 +18,22 @@ if (!empty($_POST['submit']))
 </head>
 <body>
     <div class="wrapper">
+
         <h1>My Dice</h1>
+
         <div class="dice">
             <?php foreach ($dice->getDice() as $die): ?>
             <span><?= $die; ?></span>
             <?php endforeach; ?>
         </div>
+
         <form class="roll-dice" method="POST">
             <button type="submit" name="submit">Roll Dice</button>
         </form>
+
     </div>
+
     <pre><?php var_dump($dice); ?></pre>
+
 </body>
 </html>
