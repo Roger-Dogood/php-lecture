9fc578d4c7bef9a4ef55fb13da181835e2f0611e
Roger-Dogood <daftdahl@gmail.com>
2015-02-28 17:04:05 -0800

    Added comments

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# php-lecture
+Object Oriented Approach
+------------------------
+
+This variation of the __php-lecture__ demonstrates an _object oriented_ approach to making a simple random dice php page.
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include 'Foo/Dice.php';
 
+# Creates a new instance of our Dice object.
 $dice = new Dice();
 
 if (!empty($_POST['submit']))
@@ -20,15 +21,13 @@ if (!empty($_POST['submit']))
         <h1>My Dice</h1>
         <div class="dice">
             <?php foreach ($dice->getDice() as $die): ?>
-            <span><?=$die;?></span>
+            <span><?= $die; ?></span>
             <?php endforeach; ?>
         </div>
         <form class="roll-dice" method="POST">
             <button type="submit" name="submit">Roll Dice</button>
         </form>
     </div>
-    <pre>
-    <?php var_dump($dice); ?>
-    </pre>
+    <pre><?php var_dump($dice); ?></pre>
 </body>
 </html>
